4200009f428142b50d169d6d28d25054c49dff63
KamilAdylov <farsh03@gmail.com>
2015-10-13 10:29:59 -0600

    Update Cluster.h

--- Cluster.h
+++ Cluster.h
@@ -1,9 +1,7 @@
-#include <stddef.h>
 #include "Point.h"
 
 #ifndef PA2_CLUSTER_H
 #define PA2_CLUSTER_H
-
 
 namespace Clustering {
 
@@ -17,19 +15,21 @@ namespace Clustering {
 
     class Cluster {
 
-        LNodePtr points; // node pointer to points to the points of the linked list
+        LNodePtr points;                    // node pointer to points to the points of the linked list
         int m_size;
+        static unsigned int __idGenerator;  // for use with __id
+        bool __release_points;
+        PointPtr __centroid;
+
     public:
+        static const char POINT_CLUSTER_ID_DELIM = ':';       // for use with operator<<
         int getM_size() const {
             return m_size;
         }
 
         Cluster() : m_size(0), points(nullptr) { } // default ctor
-
         Cluster(const Cluster &); // copy ctor
-
         Cluster &operator=(const Cluster &); // assignment operator
-
         ~Cluster(); // dtor
 
         void add(const PointPtr &); // add a point
@@ -40,16 +40,15 @@ namespace Clustering {
 
         Cluster &operator+=(const Point &rhs); // add point
         Cluster &operator-=(const Point &rhs); // remove point
-
         friend bool operator==(const Cluster &lhs, const Cluster &rhs);
 
-        friend std::ostream &operator<<(std::ostream &os, const Clustering::Cluster &c1);
         friend const Cluster operator+(const Cluster &lhs, const Cluster &rhs);
         friend const Cluster operator-(const Cluster &lhs, const Cluster &rhs);
-
         friend const Cluster operator+(const Cluster &lhs, const PointPtr &rhs);
         friend const Cluster operator-(const Cluster &lhs, const PointPtr &rhs);
 
+        friend std::ostream &operator<<(std::ostream &os, const Clustering::Cluster &c1);
+        friend std::istream &operator>>(std::istream &os, Clustering::Cluster &c1);
     };
 }
 #endif //PA2_CLUSTER_H
